0643d99723c8a8114fd5c0af687440172deb466b
unknown <christopher.kormpos@gmail.com>
2023-04-12 12:40:23 +0300

    newsSubHeadline,newsImgAlt,newsIsSponsored addition.NewsSections and newsTitle fix

--- index.js
+++ index.js
@@ -9,11 +9,8 @@ app.use(cors())
 
 const url = 'https://spectrum.ieee.org/type/news/'
 
-const topics = ['Aerospace', 'Artificial Intelligence', 'Biomedical', 'Computing',
-    'Consumer Electronics', 'Energy', 'History of Technology', 'Robotics',
-    'Semiconductors', 'Sensors', 'Telecommunications', 'Transportation', 'The Institute']
 
-//when we request localhost:[port] we get this response
+//when we request https://ieeespectrumapi.azurewebsites.net/api we get this response
 app.get('/api', (req, res) => {
     axios(url)
         .then(response => {
@@ -21,28 +18,46 @@ app.get('/api', (req, res) => {
             const $ = cheerio.load(html) //use of Cheerio dependency
             const articles = []
             $('.widget', html).each(function () {
-                const newsTitle = $(this).find('h2').text()
+                const newsTitle = removeSpacesAndNewlines($(this).find('h2').text())
                 const newsTimetoRead = $(this).find('.time-to-read').text() || '3 min'
+                const newsSubHeadline = $(this).find('h3').text()
                 const newsUrl = $(this).find('.widget__headline-text').attr('href')
-                const newsImgSrc = $(this).find('img').attr('data-runner-src') || '../images/nia.png' //if newsImgSrc is undefined it will set it to the default No Image Available image src
+                const newsImgSrc = $(this).find('img').attr('data-runner-src')
+                    || 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1200px-No-Image-Placeholder.svg.png'
+                //if newsImgSrc is undefined it will set it to the No Image Available png from the link
+                const newsImgAlt = $(this).find('img').attr('alt')
                 const newsDatePublished = $(this).find('.social-date').text()
-                let newsSection = $(this).find('.all-related-sections').find('a').text()
+                const newsLikes = $(this).find('.like-button').attr('data-post-likes')
+                const unfilteredSections = $(this).find('.all-related-sections').find('a').map(function () {
+                    return $(this).text();
+                }).get();
+                let newsIsSponsored = false
 
-                //check if newsSection is a probable IEEE Spectrum topic
-                for (let k = 0; k < topics.length; k++) {
-                    if (newsSection.includes(topics[k])) {
-                        newsSection = topics[k]
+                //Simple check for when an article is sponsored the newsIsSponsored value to change to true
+                for (let k = 0; k < unfilteredSections.length; k++) {
+                    if (unfilteredSections[k] === 'Sponsored Article') {
+                        newsIsSponsored = true
                     }
                 }
-
+                //Filter the unfiltered newsSections array so that the values are probable IEEE Spectrum Topics 
+                let newsSections = []
+                for (let i = 0; i < unfilteredSections.length; i++) {
+                    if (unfilteredSections[i] !== 'Type' && unfilteredSections[i] !== 'Topic' && unfilteredSections[i] !== 'News' && unfilteredSections[i] !== 'Sponsored Article') {
+                        newsSections.push(unfilteredSections[i])
+                    }
+                }
 
                 articles.push({
                     newsTitle,
                     newsUrl,
                     newsTimetoRead,
+                    newsSubHeadline,
                     newsDatePublished,
-                    newsSection,
-                    newsImgSrc
+                    newsSections,
+                    newsImgSrc,
+                    newsImgAlt,
+                    newsLikes,
+                    newsIsSponsored
                 })
             })
             //Simple check for duplicate objects if any 
@@ -52,17 +67,17 @@ app.get('/api', (req, res) => {
                         articles.splice(j, 1)
                     }
                 }
-
-
             }
-
-
-
 
             res.json(articles)
         }).catch(err => {
             res.json(err)
         })
 })
-
+//Function that removes \n and spaces from the raw strings. Function call on newsTitle
+function removeSpacesAndNewlines(str) {
+    str = str.trim();
+    str = str.replace(/\n/g, '');
+    return str;
+}
 app.listen(port, () => console.log(`Server running on port ${port}`))
